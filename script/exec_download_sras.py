45aa36483cc753b87511ea76cef19ffa43064e2b
PetaLab <34701163+petadimensionlab@users.noreply.github.com>
2019-09-04 12:00:19 +0900

    Update exec_download_sras.py

--- script/exec_download_sras.py
+++ script/exec_download_sras.py
@@ -1,8 +1,9 @@
-# http://blog.amelieff.jp/?eid=231191
 import os, shutil, glob
 
 cdir = os.getcwd()
 data_dir = '/condir'
+sra_dir = os.path.join(data_dir,'input')
+
 #prefetch_dir = '/Users/snakaoka/ws/apps/sratoolkit/bin/'
 prefetch_dir = '/usr/bin/'
 DL_dir = '/root/ncbi/public/sra/'
@@ -14,10 +15,12 @@ os.system(cmd)
 os.chdir(cdir)
 
 ## move all data to an assigned folder ##
+if not os.path.exists(sra_dir):
+        os.mkdir(sra_dir)
 os.chdir(DL_dir)
 files = glob.glob('*.sra')
 for f in files:
-    tmp = os.path.join(data_dir,'input',f)
+    tmp = os.path.join(sra_dir,f)
     shutil.move(f,tmp)
 
     
